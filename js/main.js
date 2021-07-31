92fab9caa9a30fe134e0a43ee382f7f1a8613c20
goodell5 <87952027+goodell5@users.noreply.github.com>
2021-07-31 18:13:48 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -228,7 +228,7 @@ function setupSVG() {
                 type: d3.annotationCalloutRect,
                 subject: {
                     // ROI width/height
-                    width: width * 0.83,
+                    width: width * 0.84,
                     height: height * 0.4,
                 },
                 color: ["white"],
