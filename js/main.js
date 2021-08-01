518f7aedca9c0bc1df9b38fa2d5a4c0980e7bb68
goodell5 <87952027+goodell5@users.noreply.github.com>
2021-08-01 18:04:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -181,8 +181,8 @@ function setupSVG() {
             var subgroupName = d3.select(this.parentNode).datum().key;
             var subgroupValue = d.data[subgroupName];
             console.log(groups)
-            console.log(data.obesity_level)
-            console.log(obesity_level)
+            console.log(d.data.obesity_level)
+            // console.log(obesity_level)
             if (document.getElementById("my_dataviz").getAttribute("current-slide") == "exercise") {
                 tooltip
                 .html("Weekly Exercise: " + subgroupName + "<br>" + formatSuffixDecimal2(subgroupValue) + "% of individuals")
