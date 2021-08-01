afc41c9d0d385e77e6dd52538caeecb913ca69c8
goodell5 <87952027+goodell5@users.noreply.github.com>
2021-08-01 12:05:38 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -238,7 +238,7 @@ function setupSVG() {
                 },
                 type: d3.annotationCalloutCircle,
                 subject: {
-                    radius: 80,
+                    radius: 90,
                     radiusPadding: 10, // white space around circle before connector
                 },
                 // ROI/subject x/y
