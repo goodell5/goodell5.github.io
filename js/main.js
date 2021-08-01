f1c9f4767a6892d03dd26f06b3f806c07cb9eea6
goodell5 <87952027+goodell5@users.noreply.github.com>
2021-08-01 14:59:50 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -271,7 +271,7 @@ function setupSVG() {
                 },
                 color: ["white"],
                 // ROI coords
-                x: width * 0.15,
+                x: width * 0.165,
                 y: height * 0.11,
                 // label/text coords
                 dy: 250,
@@ -309,7 +309,7 @@ function setupSVG() {
                 },
                 // ROI coords
                 x: width * 0.165,
-                y: height * 0.1,
+                y: height * 0.085,
                 // label/text coords
                 dy: 125,
                 dx: 110,
@@ -349,7 +349,7 @@ function setupSVG() {
                 },
                 color: ["white"],
                 // ROI coords
-                x: width * 0.15,
+                x: width * 0.165,
                 y: height * 0.15,
                 // label/text coords
                 dy: 250,
